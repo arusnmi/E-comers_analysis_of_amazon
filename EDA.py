b39a6b5afd1177eab6798614032a9fb11b8b201f
arusnmi <parush.larty@gmail.com>
2025-08-30 20:36:22 +0530

    done with first EDA: discounted price vs ratings

---
+++ EDA.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+
+df = pd.read_csv('amazon_discounts.csv')
+
+
+ratings = df['rating']
+discount_percentage = df['discount_percentage_number']
+
+plt.figure(figsize=(10, 6))
+plt.bar( ratings,discount_percentage)
+plt.ylabel('Discount Percentage')
+plt.xlabel('Ratings')
+plt.title('Ratings vs Discount Percentage')
+plt.xticks(rotation=45)
+plt.tight_layout()
+plt.show()
+plt.savefig('ratings_vs_discount_percentage.png')
