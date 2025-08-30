21cd542d936c162b4f8c729066fa8e2f2aae46d7
arusnmi <parush.larty@gmail.com>
2025-08-30 18:44:15 +0530

    discounted price split correctly

--- Discount splitter.py
+++ Discount splitter.py
@@ -4,6 +4,6 @@ df=pd.read_csv('amazon_fixed_ratings.csv')
 
 
 
-df ["discount_percentage_number"]= df["discount_percentage"].str.split('%').str[0].astype(float)
+df ["discount_percentage_number"]= df["discount_percentage"].str.split('%').str[0].astype(int)
 
 df.to_csv('amazon_discounts.csv', index=False)
