0767be5e59ce3530e828ebbdde792bc909956f99
arusnmi <parush.larty@gmail.com>
2025-09-03 23:59:53 +0530

    analysised every catgroy

--- EDA.py
+++ EDA.py
@@ -3,37 +3,53 @@ import matplotlib.pyplot as plt
 
 
 
-df = pd.read_csv('amazon_price_cleaned.csv')
+df = pd.read_csv('amazon_cleaned_preped.csv')
 print(df.info())
 
 
 
-#ratings vs discount percentage
-ratings = df['rating']
-discount_percentage = df['discount_percentage_number']
+# #ratings vs discount percentage
+# ratings = df['rating']
+# discount_percentage = df['discount_percentage_number']
 
-plt.figure(figsize=(10, 6))
-plt.bar( ratings,discount_percentage)
-plt.ylabel('Discount Percentage')
-plt.xlabel('Ratings')
-plt.title('Ratings vs Discount Percentage')
-plt.xticks(rotation=45)
-plt.tight_layout()
-plt.savefig('ratings_vs_discount_percentage.png')
+# plt.figure(figsize=(10, 6))
+# plt.bar( ratings,discount_percentage)
+# plt.ylabel('Discount Percentage')
+# plt.xlabel('Ratings')
+# plt.title('Ratings vs Discount Percentage')
+# plt.xticks(rotation=45)
+# plt.tight_layout()
+# plt.savefig('ratings_vs_discount_percentage.png')
 
 
-#price distribution
-discounts = df['discount_price_number']
-actuals = df['actual_price_number']
+# #price distribution
+# discounts = df['discount_price_number']
+# actuals = df['actual_price_number']
 
-plt.figure(figsize=(10, 6))
-plt.hist([discounts, actuals], bins=30, label=['Discounted Price', 'Actual Price'], alpha=0.7)
+# plt.figure(figsize=(10, 6))
+# plt.hist([discounts, actuals], bins=30, label=['Discounted Price', 'Actual Price'], alpha=0.7)
+# plt.ylabel('Frequency')
+# plt.xlabel('Price')
+# plt.title('Price Distribution')
+# plt.legend()
+# plt.tight_layout()
+# plt.savefig('price_distribution.png')
+
+
+
+#category price distrubition 
+
+categories = df['category'].unique()
+plt.figure(figsize=(12, 8))
+for category in categories:
+    category_data = df[df['category'] == category]
+    plt.hist(category_data['discount_price_number'], bins=30, alpha=0.5, label=category)
 plt.ylabel('Frequency')
-plt.xlabel('Price')
-plt.title('Price Distribution')
+plt.xlabel('Discounted Price')
+plt.title('Discounted Price Distribution by Category')
 plt.legend()
-plt.tight_layout()
-plt.savefig('price_distribution.png')
+plt.savefig('category_price_distribution.png')
+plt.show()
 
 
 
